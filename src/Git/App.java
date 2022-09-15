7a68659008b887b46898bdcf0fb0d8f874b55841
Behnke94 <behnke94@hotmail.com>
2022-09-15 11:26:11 +0200

    Initial Commit Person2 tilføjet

--- src/Git/App.java
+++ src/Git/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hej verden");
         System.out.println("Suck a Dick");
+        System.out.println("Hej");
 
     }
 }
